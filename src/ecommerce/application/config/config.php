eb460ed5289e1f4ff2a3cf87bee3911bfa2c063f
Adan1996 <syahdanmasyhuri@gmail.com>
2020-08-30 12:46:56 +0000

    change mkdir setting in Log.php

--- src/ecommerce/application/config/config.php
+++ src/ecommerce/application/config/config.php
@@ -24,7 +24,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
   | a PHP script and you can easily do that on your own.
   |
  */
-$config['base_url'] = defined('BASE_URL')?'http://192.168.1.8:5009/ecommerce/':'';
+$config['base_url'] = 'http://192.168.1.8:5009/ecommerce/';
 
 /*
   |--------------------------------------------------------------------------
